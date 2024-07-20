47fce929000c96e14485d1ad321a44c109c22282
Alrico Rizki Wibowo <alricosaiyan123@gmail.com>
2024-07-20 23:28:23 +0700

    initial commit

---
+++ src/navigation/BottomNavigation.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from '../screens/HomeScreen';
+import DetailsScreen from '../screens/DetailsScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+type RootStackParamList = {
+  Home: undefined;
+  Details: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Details" component={DetailsScreen} />
+    </Stack.Navigator>
+  );
+};
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigator: React.FC = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={StackNavigator} />
+      {/* Tambahkan tab lain jika diperlukan */}
+    </Tab.Navigator>
+  );
+};
+
+const AppNavigator: React.FC = () => {
+  return (
+    <NavigationContainer>
+      <BottomTabNavigator />
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
